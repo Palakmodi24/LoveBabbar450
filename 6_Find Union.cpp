b873de5b0560ffa42ca9f5930f105ba6625357d7
Palak <modipalak62@gmail.com>
2025-07-23 23:26:31 +0530

    Added intersection code

--- 6_Find Union.cpp
+++ 6_Find Union.cpp
@@ -32,3 +32,23 @@ class Solution {
 
 
 //lLogic is set contains only uniques values
+
+
+//Added intersection code
+vector<int> findIntersection(vector<int>& nums1, vector<int>& nums2) {
+    unordered_set<int> temp;
+    unordered_set<int> result;
+
+     // copy from a and insert to temp
+        for (int i = 0; i < a.size(); i++) {
+            temp.insert(a[i]);
+        }
+        for (int num : nums2) {
+            if (temp.count(num)) {
+                result.insert(num);
+            }
+        }
+    
+
+    return vector<int>(result.begin(), result.end());
+}
